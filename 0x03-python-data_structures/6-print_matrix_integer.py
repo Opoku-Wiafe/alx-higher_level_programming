e0612b0af04c5f0ab5d8f03e87522038bc7399d8
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-11-07 20:23:59 -0800

    first correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for elemen in matrix:
         for elemen2 in elemen:
             print("{:d}".format(elemen2),
-                    end="" if elemen[len(elemen)-1] == elemen2 else " ")
+                  end="" if elemen[len(elemen)-1] == elemen2 else " ")
         print("".format())
