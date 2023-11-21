ff60bc92a7db80635003f84e1e17363593194488
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-11-20 19:21:43 -0800

    safe division func

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,4 +13,4 @@ def safe_print_list_integers(my_list=[], x=0):
     if (firstCount + secondCount < x):
         raise IndexError("list index out of range")
     print("".format())
-    return firstCount 
+    return firstCount
