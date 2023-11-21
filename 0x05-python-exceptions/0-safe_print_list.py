319206831239aaac0b436d2d71c0a3ae9bb9093a
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-11-20 18:30:58 -0800

    print counter with except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+ #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in my_list:
+            if (count >= x):
+                break
+            print("{}".format(i), end="")
+            count += 1
+        print("".format())
+        return count
+    except ValueError:
+        print("ValueError error")
+
