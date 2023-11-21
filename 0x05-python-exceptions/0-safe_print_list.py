3aea4603f4e0284f1626b54565eb17994e3eb227
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-11-20 18:40:12 -0800

    print counter with except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -11,4 +11,3 @@ def safe_print_list(my_list=[], x=0):
         return count
     except ValueError:
         print("ValueError error")
-
