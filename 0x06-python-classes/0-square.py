5c808aad49145de44c092515cd9dcfab78bfcce9
Opoku-Wiafe <opokuwiafevan@gmail.com>
2023-11-21 20:51:40 -0800

    first class python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Square:
     pass
-if __name__ ==  "__main--":
-        my_square = Square()
-        print(type(my_square))
+
+
+if __name__ == "__main--":
+    my_square = Square()
+    print(type(my_square))
