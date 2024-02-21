421b624f5b9a3c859b6a12698bb1c3f6198c449d
OPOKU WIAFE STEPHEN <opokuwiafevan@gmail.com>
2024-02-21 04:20:45 +0000

    fixed

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- deletes the database hbtn_0c_0 in your MySQL server.
 -- cat 1-create_database_if_missing.sql | sudo mysql -hlocalhost -uroot -p
-DELETE DATABASE IF NOT EXISTS hbtn_0c_0;
+DELETE DATABASE IF EXISTS hbtn_0c_0;
