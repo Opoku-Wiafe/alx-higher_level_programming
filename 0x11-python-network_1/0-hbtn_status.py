f800cf56993cc4a5c0524429263673b0361d8080
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-03-02 14:33:27 -0800

    online data status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ import urllib.request
 if __name__ == "__main__":
     response = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(response) as get_response:
-        utf_data = get_response.read().decode('utf-8')
-        resourceType = type(get_response.read())
+        data = get_response.read()
+        utf_data = data.decode('utf-8')
+        resourceType = type(data)
         print(f"Body response:\n\t- type: {resourceType}")
-        print(f"\t- content: {get_response.read()}")
+        print(f"\t- content: {data}")
         print(f"\t- utf8 content: {utf_data}")
-
